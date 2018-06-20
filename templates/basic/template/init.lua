f3a526dd4546c69ca8ffa939b23f50d7299e58bf
igorcoding <igorcoding@gmail.com>
2018-06-20 18:56:07 +0300

    path tweaks

--- templates/basic/template/init.lua
+++ templates/basic/template/init.lua
@@ -1,11 +1,12 @@
 #! /usr/bin/env tarantool
 
-local function script_path() local fio = require'fio' local b = debug.getinfo(2, "S").source:sub(2) local lb = fio.readlink(b) if lb ~= nil then b = lb end return b:match("(.*/)") end
+local soext = (jit.os == "OSX" and "dylib" or "so")
+local function script_path() local fio = require "fio"; local b = debug.getinfo(2, "S").source:sub(2); local b_dir = fio.dirname(b); local lb = fio.readlink(b); while lb ~= nil do if not string.startswith(lb, '/') then lb = fio.abspath(fio.pathjoin(b_dir, lb)) end; b = lb; lb = fio.readlink(b) end return b:match("(.*/)") end
 local function addpaths(dst, ...) local cwd = script_path() or fio.cwd() .. "/"; local pp = {}; for s in package[dst]:gmatch("([^;]+)") do pp[s] = 1 end; local add = ""; for _, p in ipairs({...}) do local ap = cwd .. p; if string.startswith(p, '/') then ap = p end; if not pp[ap] then add = add .. ap .. ";" end; end package[dst] = add .. package[dst] return end
 addpaths('path', '?.lua', '?/init.lua', 'app/?.lua', 'app/?/init.lua', '.rocks/share/lua/5.1/?.lua', '.rocks/share/lua/5.1/?/init.lua', '.rocks/share/tarantool/?.lua', '.rocks/share/tarantool/?/init.lua')
-addpaths('cpath', '.rocks/lib/lua/5.1/?.so', '.rocks/lib/lua/?.so', '.rocks/lib64/lua/5.1/?.so', '.rocks/lib/tarantool/?.so', '.rocks/lib64/tarantool/?.so',
-                  '.rocks/lib/lua/5.1/?.dylib', '.rocks/lib/lua/?.dylib', '.rocks/lib64/lua/5.1/?.dylib', '.rocks/lib/tarantool/?.dylib', '.rocks/lib64/tarantool/?.dylib')
+addpaths('cpath', '.rocks/lib/lua/5.1/?.'..soext, '.rocks/lib/lua/?.'..soext, '.rocks/lib64/lua/5.1/?.'..soext, '.rocks/lib/tarantool/?.'..soext, '.rocks/lib64/tarantool/?.'..soext)
 
+require 'strict'.on()
 require 'package.reload'
 local fio = require 'fio'
 
@@ -15,17 +16,16 @@ if conf_path == nil then
 end
 local conf = require('config')(conf_path)
 
-require 'strict'.on()
 local app = require 'app'
 if app ~= nil and app.init ~= nil then
     local tb
     local ok, res = xpcall(
-        function() 
-            return app.init(conf.get('app')) 
-        end, 
-        function(err) 
-            tb = debug.traceback(); 
-            return err 
+        function()
+            return app.init(conf.get('app'))
+        end,
+        function(err)
+            tb = debug.traceback();
+            return err
         end
     )
 
