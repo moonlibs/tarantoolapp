21a4de3dfb42c0c98dc15bfc12a4201bf13304fe
igorcoding <igorcoding@gmail.com>
2018-06-27 13:43:26 +0300

    Added proper versions checks in t/init.lua. Fixes #1.

--- templates/vshard/template/t/tnt/init.lua
+++ templates/vshard/template/t/tnt/init.lua
@@ -11,6 +11,49 @@ if dir == nil then
     cleanup = true
 end
 
+local function compare_versions(expected, version)
+    -- from tarantool/queue compat.lua
+    local fun = require 'fun'
+    local iter, op  = fun.iter, fun.operator
+
+    local function split(self, sep)
+        local sep, fields = sep or ":", {}
+        local pattern = string.format("([^%s]+)", sep)
+        self:gsub(pattern, function(c) table.insert(fields, c) end)
+        return fields
+    end
+
+    local function reducer(res, l, r)
+        if res ~= nil then
+            return res
+        end
+        if tonumber(l) == tonumber(r) then
+            return nil
+        end
+        return tonumber(l) > tonumber(r)
+    end
+
+    local function split_version(version_string)
+        local vtable  = split(version_string, '.')
+        local vtable2 = split(vtable[3],  '-')
+        vtable[3], vtable[4] = vtable2[1], vtable2[2]
+        return vtable
+    end
+
+    local function check_version(expected, version)
+        version = version or _TARANTOOL
+        if type(version) == 'string' then
+            version = split_version(version)
+        end
+        local res = iter(version):zip(expected):reduce(reducer, nil)
+
+        if res or res == nil then res = true end
+        return res
+    end
+
+    return check_version(expected, version)
+end
+
 local function tnt_prepare(cfg_args)
     cfg_args = cfg_args or {}
     local files = fio.glob(fio.pathjoin(dir, '*'))
@@ -21,7 +64,7 @@ local function tnt_prepare(cfg_args)
         end
     end
 
-    if require('tarantool').version >= "1.7.3" then
+    if compare_versions({1, 7, 3}, _TARANTOOL) then
         cfg_args['memtx_dir']  = dir
         cfg_args['log']        = "file:" .. fio.pathjoin(dir, 'tarantool.log')
     else
