20195e21d6ba958ef3da22e8371b8722326ac7a8
bofm <bofm@github.com>
2018-07-03 12:57:00 +0300

    paths - basic

--- templates/basic/template/scripts/test.sh
+++ templates/basic/template/scripts/test.sh
@@ -7,13 +7,15 @@ TARANTOOL=tarantool
 LUA_PATH="\
 ${ROOT}/.rocks/share/lua/5.1/?.lua;\
 ${ROOT}/.rocks/share/lua/5.1/?/init.lua;\
+${ROOT}/.rocks/share/tarantool/?.lua;\
+${ROOT}/.rocks/share/tarantool/?/init.lua;\
 ${ROOT}/?.lua;\
 ${ROOT}/?/init.lua;\
 ${ROOT}/app/?.lua;\
 ${ROOT}/app/?/init.lua;\
-"
+;"
 
-SOEXT="$(${TARANTOOL} <<< 'print(jit.os == "OSX" and "dylib" or "so")')"
+SOEXT="$(${TARANTOOL} <<< print(jit.os == "OSX" and "dylib" or "so"))"
 
 LUA_CPATH="\
 ${ROOT}/.rocks/lib/lua/5.1/?.${SOEXT};\
@@ -21,7 +23,7 @@ ${ROOT}/.rocks/lib/lua/?.${SOEXT};\
 ${ROOT}/.rocks/lib/tarantool/?.${SOEXT};\
 ${ROOT}/.rocks/lib64/lua/5.1/?.${SOEXT};\
 ${ROOT}/.rocks/lib64/tarantool/?.${SOEXT};\
-"
+;"
 
 for t in ${ROOT}/t/*.lua; do
     echo "Running `basename $t`..."
